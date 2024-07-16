84ae2c8d499eab8d8ed91d78c4ef2d12a51759e8
Janek Karaszwski <jakarwski19@gmail.com>
2024-07-16 23:19:09 +0200

    Fix metals config

--- nvim/.config/nvim/lua/janekkar/lazy/metals.lua
+++ nvim/.config/nvim/lua/janekkar/lazy/metals.lua
@@ -2,39 +2,6 @@ local map = vim.keymap.set
 local fn = vim.fn
 
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-vsnip" },
-      { "hrsh7th/vim-vsnip" }
-    },
-    opts = function()
-      local cmp = require("cmp")
-      local conf = {
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "vsnip" },
-        },
-        snippet = {
-          expand = function(args)
-            -- Comes from vsnip
-            fn["vsnip#anonymous"](args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          -- None of this made sense to me when first looking into this since there
-          -- is no vim docs, but you can't have select = true here _unless_ you are
-          -- also using the snippet stuff. So keep in mind that if you remove
-          -- snippets you need to remove this select
-          ["<CR>"] = cmp.mapping.confirm({ select = true })
-        })
-      }
-      return conf
-    end
-  },
-  {
     "scalameta/nvim-metals",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -182,6 +149,5 @@ return {
       vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
     end
 
-  }
 }
 
