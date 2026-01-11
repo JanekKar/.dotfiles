f82bf69b19bf80318d03df7d89020600fd84e7c6
Janek Karaszewski <jakarwski19@gmail.com>
2026-01-11 11:48:45 +0100

    fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,13 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+starship init fish | source
+zoxide init --cmd cd fish | source
+source ~/.alias
+
+
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
